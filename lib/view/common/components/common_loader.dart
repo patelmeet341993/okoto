f0abfcf0f8bc4897c1b0217cfd0bb88c8154c681
Dishant <agrawaldishant29@gmail.com>
2023-02-06 18:19:25 +0530

    added 'center' configuration in CommonLoader

--- lib/view/common/components/common_loader.dart
+++ lib/view/common/components/common_loader.dart
@@ -4,18 +4,31 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 class CommonLoader extends StatelessWidget {
   final double size;
   final Color? backgroundColor, ringColor;
+  final bool isCenter;
 
   const CommonLoader({
     Key? key,
     this.size = 80,
     this.backgroundColor,
     this.ringColor,
+    this.isCenter = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
 
+    if(isCenter) {
+      return Center(
+        child: getMainBody(themeData: themeData),
+      );
+    }
+    else {
+      return getMainBody(themeData: themeData);
+    }
+  }
+
+  Widget getMainBody({required ThemeData themeData}) {
     return Container(
       padding: EdgeInsets.all(size * 0.25),
       width: size,
