2fdd48da324c71eb11834a3488be7eda8c444ddd
Dishant <agrawaldishant29@gmail.com>
2023-02-14 16:24:33 +0530

    added createOrderForSubscription and createOrderForProduct methods in OrderController to create order for Subscription and Product

--- lib/backend/order/order_controller.dart
+++ lib/backend/order/order_controller.dart
@@ -1,3 +1,10 @@
+import 'package:okoto/configs/constants.dart';
+import 'package:okoto/model/order/order_model.dart';
+import 'package:okoto/model/product/product_model.dart';
+import 'package:okoto/model/subscription/subscription_model.dart';
+import 'package:okoto/utils/my_print.dart';
+import 'package:okoto/utils/my_utils.dart';
+
 import 'order_provider.dart';
 import 'order_repository.dart';
 
@@ -12,5 +19,53 @@ class OrderController {
 
   OrderProvider get orderProvider => _orderProvider;
 
+  Future<bool> createOrderForSubscription({required SubscriptionModel subscriptionModel, required String paymentId, required String paymentMode,
+    required String paymentStatus, required double amount}) async {
+    String tag = MyUtils.getUniqueIdFromUuid();
+    MyPrint.printOnConsole("OrderController().createOrderForSubscription() called with subscriptionModel:'$subscriptionModel', paymentId:'$paymentId', "
+        "paymentMode:'$paymentMode', paymentStatus:'$paymentStatus', amount:'$amount'", tag: tag);
 
+    bool isOrderForSubscriptionCreated = false;
+
+    OrderModel orderModel = OrderModel(
+      id: MyUtils.getUniqueIdFromUuid(),
+      type: OrderType.subscription,
+      paymentId: paymentId,
+      paymentMode: paymentMode,
+      paymentStatus: paymentStatus,
+      amount: amount,
+      subscriptionOrderDataModel: subscriptionModel,
+    );
+
+    isOrderForSubscriptionCreated = await _orderRepository.createOrderInFirestoreFromOrderModel(orderModel: orderModel);
+
+    MyPrint.printOnConsole("isOrderForSubscriptionCreated:'$isOrderForSubscriptionCreated'", tag: tag);
+
+    return isOrderForSubscriptionCreated;
+  }
+
+  Future<bool> createOrderForProduct({required ProductModel productModel, required String paymentId, required String paymentMode,
+    required String paymentStatus, required double amount}) async {
+    String tag = MyUtils.getUniqueIdFromUuid();
+    MyPrint.printOnConsole("OrderController().createOrderForProduct() called with productModel:'$productModel', paymentId:'$paymentId', "
+        "paymentMode:'$paymentMode', paymentStatus:'$paymentStatus', amount:'$amount'", tag: tag);
+
+    bool isOrderForProductCreated = false;
+
+    OrderModel orderModel = OrderModel(
+      id: MyUtils.getUniqueIdFromUuid(),
+      type: OrderType.product,
+      paymentId: paymentId,
+      paymentMode: paymentMode,
+      paymentStatus: paymentStatus,
+      amount: amount,
+      productOrderDataModel: productModel,
+    );
+
+    isOrderForProductCreated = await _orderRepository.createOrderInFirestoreFromOrderModel(orderModel: orderModel);
+
+    MyPrint.printOnConsole("isOrderForProductCreated:'$isOrderForProductCreated'", tag: tag);
+
+    return isOrderForProductCreated;
+  }
 }
