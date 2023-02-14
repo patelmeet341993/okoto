8f19283ca48c4e8c6d804f8702edcf848845b3c5
Dishant <agrawaldishant29@gmail.com>
2023-02-14 16:26:50 +0530

    added buySubscription and addDummySubscription methods in SubscriptionController

--- lib/backend/subscription/subscription_controller.dart
+++ lib/backend/subscription/subscription_controller.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:okoto/backend/order/order_controller.dart';
 import 'package:okoto/backend/subscription/subscription_provider.dart';
 import 'package:okoto/backend/subscription/subscription_repository.dart';
+import 'package:okoto/backend/user/user_controller.dart';
+import 'package:okoto/backend/user/user_provider.dart';
 import 'package:okoto/model/subscription/subscription_model.dart';
+import 'package:okoto/utils/my_toast.dart';
 
 import '../../utils/my_print.dart';
 import '../../utils/my_utils.dart';
@@ -46,5 +52,111 @@ class SubscriptionController {
     MyPrint.printOnConsole("SubscriptionController().getAllSubscriptionsList() Finished", tag: tag);
   }
 
+  Future<bool> buySubscription({required BuildContext context, required SubscriptionModel subscriptionModel, required String userId,
+    required UserProvider userProvider}) async {
+    bool isBuySuccessful = false;
 
+    if(userId.isEmpty) {
+      return isBuySuccessful;
+    }
+
+    bool isPaymentSuccess = false;
+    String paymentId = "";
+
+    double amount = subscriptionModel.price;
+
+    //region TODO: Comment Below Code. This is only for testing
+    paymentId = paymentId;
+    isPaymentSuccess = true;
+    await Future.delayed(const Duration(seconds: 3));
+    if(context.mounted) MyToast.showSuccess(context: context, msg: "Payment Success");
+    //endregion
+
+    //region TODO: Uncomment Below Code. This is real payment code
+    /*await PaymentController().createPayment(
+      amount: amount,
+      handleSuccess: (String paymentId, String orderId, String paymentSignature) async {
+        paymentId = paymentId;
+        isPaymentSuccess = true;
+
+        MyToast.showSuccess(context: context, msg: "Payment Success");
+      },
+      handleCancel: (String message) {
+        isPaymentSuccess = false;
+        MyPrint.printOnConsole("Payment Canceled Message:$message");
+        MyToast.showError(context: context, msg: "Payment Cancelled");
+      },
+      handleFailure: () {
+        isPaymentSuccess = false;
+        MyToast.showError(context: context, msg: "Payment Failed");
+      },
+    );*/
+    //endregion
+
+    if(isPaymentSuccess == true) {
+      bool isOrderCreated = await OrderController(orderProvider: null).createOrderForSubscription(
+        subscriptionModel: subscriptionModel,
+        paymentId: paymentId,
+        paymentMode: "Razorpay",
+        paymentStatus: "Completed",
+        amount: amount,
+      );
+
+      if(!isOrderCreated) {
+        if(context.mounted) {
+          MyToast.showError(context: context, msg: "Couldn't Place Order for Subscription");
+        }
+        return isBuySuccessful;
+      }
+
+      bool isSubscriptionActivated = await _subscriptionRepository.activateSubscriptionForUser(subscriptionModel: subscriptionModel, userId: userId);
+
+      if(isSubscriptionActivated) {
+        isBuySuccessful = true;
+
+        await UserController(userProvider: userProvider).getUserDataAndStoreInProvider(userId: userId);
+
+        if(context.mounted) {
+          MyToast.showSuccess(context: context, msg: "Subscription Activated Successfully");
+        }
+      }
+      else {
+        if(context.mounted) {
+          MyToast.showError(context: context, msg: "Couldn't Place Order for Subscription");
+        }
+      }
+    }
+
+    return isBuySuccessful;
+  }
+
+  Future<void> checkSubscriptionActive() async {
+
+  }
+
+
+
+  Future<bool> addDummySubscription() async {
+    String tag = MyUtils.getUniqueIdFromUuid();
+    MyPrint.printOnConsole("SubscriptionController().addDummySubscription() called", tag: tag);
+
+    bool isSubscriptionAdded = false;
+
+    isSubscriptionAdded = await _subscriptionRepository.createSubscriptionInFirestoreFromModel(subscriptionModel: SubscriptionModel(
+      id: MyUtils.getUniqueIdFromUuid(),
+      name: "Plan 3",
+      createdTime: Timestamp.now(),
+      discountedPrice: -1,
+      price: 250,
+      enabled: true,
+      gamesList: [
+        "qKSXQtym6CcUNTwGv0hQ"
+      ],
+      validityInDays: 28,
+    ));
+
+    MyPrint.printOnConsole("isSubscriptionAdded:'$isSubscriptionAdded'", tag: tag);
+
+    return isSubscriptionAdded;
+  }
 }
