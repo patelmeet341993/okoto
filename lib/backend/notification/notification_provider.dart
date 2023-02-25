a2fbdc793482a36cbae9a1afe605e9f6465556e3
Dishant <agrawaldishant29@gmail.com>
2023-02-25 17:35:08 +0530

    added notificationToken in NotificationProvider

--- lib/backend/notification/notification_provider.dart
+++ lib/backend/notification/notification_provider.dart
@@ -1,5 +1,16 @@
 import 'package:okoto/backend/common/common_provider.dart';
 
 class NotificationProvider extends CommonProvider {
+  NotificationProvider() {
+    _initializeFields();
+  }
 
+  void _initializeFields() {
+    notificationToken = CommonProviderPrimitiveParameter<String?>(
+      value: null,
+      notify: notify,
+    );
+  }
+
+  late final CommonProviderPrimitiveParameter<String?> notificationToken;
 }
