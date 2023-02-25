d34988123ea70843b875982c31de5491d3a7b567
hpy <happypatel379@gmail.com>
2023-02-25 12:22:29 +0530

    change design of the list view and card

--- lib/view/subscription/components/subscription_card.dart
+++ lib/view/subscription/components/subscription_card.dart
@@ -1,6 +1,13 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:okoto/model/subscription/subscription_model.dart';
+import 'package:okoto/utils/my_utils.dart';
+import 'package:okoto/view/common/components/common_cachednetwork_image.dart';
+import 'package:okoto/view/common/components/common_submit_button.dart';
+import 'package:okoto/view/common/components/common_text.dart';
 
+import '../../../configs/styles.dart';
 import '../../common/components/common_button1.dart';
 
 class SubscriptionCard extends StatelessWidget {
@@ -16,11 +23,17 @@ class SubscriptionCard extends StatelessWidget {
     this.buySubscriptionCallback,
   }) : super(key: key);
 
+  int getDiscountPercentage(double netPrice, double salePrice) {
+    double discountPercent = 0;
+    discountPercent = ((netPrice - salePrice) / netPrice) * 100;
+    return discountPercent.roundToDouble().toInt();
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget widget = getMainBody();
 
-    if(isActiveSubscription) {
+    if (isActiveSubscription) {
       widget = ClipRRect(
         child: Banner(
           message: 'Active',
@@ -31,37 +44,174 @@ class SubscriptionCard extends StatelessWidget {
       );
     }
 
-    widget = Container(
-      margin: const EdgeInsets.symmetric(vertical: 5),
-      child: widget,
-    );
+    widget = Container(margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 18), child: widget);
 
     return widget;
   }
 
   Widget getMainBody() {
-    return Card(
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20).copyWith(bottom: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+          colors: [
+            Styles.cardGradient1,
+            Styles.cardGradient2,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
       child: Column(
         children: [
-          Text(subscriptionModel.id),
-          Text(subscriptionModel.name),
-          Text("Price: \$${subscriptionModel.price}"),
-          Text("Validity: ${subscriptionModel.validityInDays} Days"),
-          Text("Games: ${subscriptionModel.gamesList}"),
-          const SizedBox(height: 10,),
-          if(showBuyButton) Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            child: CommonButton1(
-              onTap: () async {
-                if(buySubscriptionCallback != null) {
-                  await buySubscriptionCallback!(subscriptionModel: subscriptionModel);
-                }
-              },
-              text: "Choose",
-            ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(elevation: 8, child: getImage(url: subscriptionModel.image)),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CommonText(
+                      text: subscriptionModel.name,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                      letterSpacing: 0.5,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    CommonText(
+                      text: "Validity: ${subscriptionModel.validityInDays} Days",
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.5,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Visibility(
+                      visible: subscriptionModel.discountedPrice == 0,
+                      child: CommonText(text: "Rs. ${subscriptionModel.price}", letterSpacing: 0.5, fontWeight: FontWeight.w600),
+                    ),
+                    Visibility(
+                      visible: subscriptionModel.discountedPrice != 0,
+                      child: Row(
+                        children: [
+                          CommonText(text: "Rs. ${subscriptionModel.discountedPrice}", letterSpacing: 0.5, fontWeight: FontWeight.w600),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          CommonText(
+                            text: "Rs. ${subscriptionModel.price}",
+                            letterSpacing: 0.5,
+                            fontWeight: FontWeight.w400,
+                            textDecoration: TextDecoration.lineThrough,
+                            fontSize: 12,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Container(
+                              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                              decoration: BoxDecoration(color: Styles.discountCardColor, borderRadius: BorderRadius.circular(20)),
+                              child: Center(
+                                  child: CommonText(
+                                text: "${getDiscountPercentage(subscriptionModel.price, subscriptionModel.discountedPrice)}% OFF",
+                                letterSpacing: 0.2,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 9,
+                                color: Styles.discountTextColor,
+                                textAlign: TextAlign.center,
+                              ))),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: showBuyButton ? 10:20,
+          ),
+          Row(
+            children: [
+              Expanded(child: getGameListWidget()),
+              if (showBuyButton)
+                CommonSubmitButton(
+                  onTap: () async {
+                    if (buySubscriptionCallback != null) {
+                      await buySubscriptionCallback!(subscriptionModel: subscriptionModel);
+                    }
+                  },
+                  text: "Choose",
+                  fontSize: 12,
+                  verticalPadding: 6,
+                  horizontalPadding: 15,
+                  borderRadius: 5,
+                ),
+            ],
           ),
         ],
       ),
+    );
+  }
+
+  Widget getGameListWidget() {
+    return subscriptionModel.gamesList.length <= 3
+        ? Row(
+            children: List.generate(
+              subscriptionModel.gamesList.length,
+              (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: getImage(url: subscriptionModel.gamesList[index], borderRadius: 50, height: 20, width: 20),
+                );
+              },
+            ),
+          )
+        : Row(
+            children: [
+              Container(
+                height: 20,
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 4.0),
+                      child: getImage(url: subscriptionModel.gamesList[index], borderRadius: 50, height: 20, width: 20),
+                    );
+                  },
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+              SizedBox(
+                width: 6,
+              ),
+              CommonText(
+                text: "+${subscriptionModel.gamesList.length - 3} more",
+                fontSize: 9,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 0.3,
+              )
+            ],
+          );
+  }
+
+  Widget getImage({String url = "", double borderRadius = 5.0, double height = 76, double width = 76}) {
+    return CommonCachedNetworkImage(
+      imageUrl: MyUtils().getSecureUrl(url),
+      height: height,
+      width: width,
+      borderRadius: borderRadius,
     );
   }
 }
--- lib/view/subscription/screens/subscription_list_screen.dart
+++ lib/view/subscription/screens/subscription_list_screen.dart
@@ -4,8 +4,10 @@ import 'package:okoto/backend/subscription/subscription_controller.dart';
 import 'package:okoto/backend/user/user_provider.dart';
 import 'package:okoto/model/subscription/subscription_model.dart';
 import 'package:okoto/utils/my_toast.dart';
+import 'package:okoto/view/common/components/common_appbar.dart';
 import 'package:okoto/view/common/components/common_loader.dart';
 import 'package:okoto/view/common/components/modal_progress_hud.dart';
+import 'package:okoto/view/common/components/my_screen_background.dart';
 import 'package:provider/provider.dart';
 
 import '../../../backend/subscription/subscription_provider.dart';
@@ -83,41 +85,36 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
   Widget build(BuildContext context) {
     themeData = Theme.of(context);
 
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<SubscriptionProvider>.value(value: subscriptionProvider),
-        ChangeNotifierProvider<UserProvider>.value(value: userProvider),
-      ],
-      child: Consumer2<SubscriptionProvider, UserProvider>(
-        builder: (BuildContext context, SubscriptionProvider subscriptionProvider, UserProvider userProvider, Widget? child) {
-          return ModalProgressHUD(
-            inAsyncCall: isLoading,
-            progressIndicator: const CommonLoader(),
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text(
-                  "Subscriptions",
-                  style: TextStyle(
-                    // fontSize: 16,
+    return MyScreenBackground(
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<SubscriptionProvider>.value(value: subscriptionProvider),
+          ChangeNotifierProvider<UserProvider>.value(value: userProvider),
+        ],
+        child: Consumer2<SubscriptionProvider, UserProvider>(
+          builder: (BuildContext context, SubscriptionProvider subscriptionProvider, UserProvider userProvider, Widget? child) {
+            return ModalProgressHUD(
+              inAsyncCall: isLoading,
+              progressIndicator: const CommonLoader(),
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: const CommonAppBar(text: "Subscriptions",),
+                body: SafeArea(
+                  child: getMainBody(
+                    subscriptionProvider: subscriptionProvider,
+                    userProvider: userProvider,
                   ),
                 ),
               ),
-              body: SafeArea(
-                child: getMainBody(
-                  subscriptionProvider: subscriptionProvider,
-                  userProvider: userProvider,
-                ),
-              ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
 
   Widget getMainBody({required SubscriptionProvider subscriptionProvider, required UserProvider userProvider}) {
     SubscriptionModel? activeSubscriptionModel = userProvider.getUserModel()?.userSubscriptionModel?.mySubscription;
-
     return Column(
       children: [
         // getActiveSubscriptionWidget(activeSubscriptionModel: activeSubscriptionModel),
@@ -179,7 +176,7 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
         await getSubscriptions(isRefresh: true, isNotify: true);
       },
       child: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 10).copyWith(top: 40),
         itemCount: subscriptions.length,
         itemBuilder: (BuildContext context, int index) {
           SubscriptionModel subscriptionModel = subscriptions[index];
