2664c5a7c7a7f63efa1cfa2f773bae61e57ff05b
hpy <happypatel379@gmail.com>
2023-03-02 12:10:29 +0530

    Completed profile screen design.

--- lib/view/profile_screen/screens/profile_screen.dart
+++ lib/view/profile_screen/screens/profile_screen.dart
@@ -46,51 +46,57 @@ class _ProfileScreenState extends State<ProfileScreen> {
         inAsyncCall: isLoading,
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          appBar:  CommonAppBar(text: "Profile", actions: [
+          appBar: CommonAppBar(text: "Profile", actions: [
             InkWell(
-                onTap: (){
-
+                onTap: () {
                   AuthenticationController(userProvider: userProvider).logout(context: context, isShowConfirmDialog: true);
-
                 },
-                child: const Icon(Icons.logout,color: Colors.white,size: 24,)),
-            SizedBox(width: 20,),
+                child: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                  size: 24,
+                )),
+            const SizedBox(
+              width: 20,
+            ),
           ]),
           body: Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 33,
-                ),
-                getMainWidget(),
-                // Center(
-                //   child: Column(
-                //     mainAxisSize: MainAxisSize.min,
-                //     children: [
-                //       singleOption1(
-                //         iconData: Icons.list,
-                //         option: "Orders",
-                //         ontap: () async {
-                //           NavigationController.navigateToOrderListScreen(
-                //               navigationOperationParameters: NavigationOperationParameters(
-                //             context: context,
-                //             navigationType: NavigationType.pushNamed,
-                //           ));
-                //         },
-                //       ),
-                //       singleOption1(
-                //         iconData: Icons.logout,
-                //         option: "Logout",
-                //         ontap: () async {
-                //           AuthenticationController(userProvider: userProvider).logout(context: context, isShowConfirmDialog: true);
-                //         },
-                //       ),
-                //     ],
-                //   ),
-                // ),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 33,
+                  ),
+                  getMainWidget(),
+                  // Center(
+                  //   child: Column(
+                  //     mainAxisSize: MainAxisSize.min,
+                  //     children: [
+                  //       singleOption1(
+                  //         iconData: Icons.list,
+                  //         option: "Orders",
+                  //         ontap: () async {
+                  //           NavigationController.navigateToOrderListScreen(
+                  //               navigationOperationParameters: NavigationOperationParameters(
+                  //             context: context,
+                  //             navigationType: NavigationType.pushNamed,
+                  //           ));
+                  //         },
+                  //       ),
+                  //       singleOption1(
+                  //         iconData: Icons.logout,
+                  //         option: "Logout",
+                  //         ontap: () async {
+                  //           AuthenticationController(userProvider: userProvider).logout(context: context, isShowConfirmDialog: true);
+                  //         },
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
           ),
         ),
@@ -107,10 +113,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
             getProfileUserNameWidget(
               userProvider.getUserModel() ?? UserModel(),
             ),
-            SizedBox(height: 33,),
+            const SizedBox(
+              height: 33,
+            ),
             getLastGamePlayedWidget(
               userProvider.getUserModel() ?? UserModel(),
-            )
+            ),
+            const SizedBox(
+              height: 33,
+            ),
+            getAccountDetailWidget()
           ],
         );
       },
@@ -121,7 +133,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget getProfileUserNameWidget(UserModel userModel) {
     return Column(
       children: [
-        MyProfileAvatar(
+        const MyProfileAvatar(
           size: 100,
         ),
         CommonText(
@@ -157,8 +169,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
           Expanded(
             child: getHeaderWithValue(headerText: "Last game played", value: "Call of Duty: Warzone", iconPath: "assets/icons/gamePad.png"),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
             child: VerticalDivider(
               thickness: 2,
               color: Colors.white,
@@ -186,7 +198,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           fontStyle: FontStyle.italic,
           fontWeight: FontWeight.w600,
         ),
-        SizedBox(
+        const SizedBox(
           height: 16,
         ),
         Padding(
@@ -218,6 +230,181 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ),
       ],
+    );
+  }
+
+  //endregion
+
+  //region accountDetailWidget
+  Widget getAccountDetailWidget() {
+    return Container(
+      child: Column(
+        children: [
+          commonDetailWidget(
+            title: "Account Details",
+            firstModel: IconWithTitleAndOnTapModel(
+              title: "Edit Details",
+              assetPath: "assets/icons/personalInfo.png",
+            ),
+            secondModel: IconWithTitleAndOnTapModel(
+              title: "Device List",
+              onTap: () {
+                NavigationController.navigateToDevicesScreen(
+                    navigationOperationParameters: NavigationOperationParameters(
+                  context: NavigationController.mainScreenNavigator.currentContext!,
+                  navigationType: NavigationType.pushNamed,
+                ));
+              },
+              assetPath: "assets/icons/deviceList.png",
+            ),
+            thirdModel: IconWithTitleAndOnTapModel(
+              title: "Payment History",
+              onTap: () {
+                NavigationController.navigateToOrderListScreen(
+                    navigationOperationParameters: NavigationOperationParameters(
+                  context: context,
+                  navigationType: NavigationType.pushNamed,
+                ));
+              },
+              assetPath: "assets/icons/paymentHistory.png",
+            ),
+          ),
+          const SizedBox(
+            height: 33,
+          ),
+          commonDetailWidget(
+            title: "More",
+            firstModel: IconWithTitleAndOnTapModel(
+              title: "About the company",
+              assetPath: "assets/icons/about.png",
+            ),
+            secondModel: IconWithTitleAndOnTapModel(
+              title: "Privacy Policy",
+              assetPath: "assets/icons/pp.png",
+            ),
+            thirdModel: IconWithTitleAndOnTapModel(
+              title: "Logout",
+              onTap: () {
+                AuthenticationController(userProvider: userProvider).logout(context: context, isShowConfirmDialog: true);
+              },
+              assetPath: "assets/icons/logout.png",
+            ),
+          ),
+          const SizedBox(
+            height: 33,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget commonDetailWidget(
+      {required String title, IconWithTitleAndOnTapModel? firstModel, IconWithTitleAndOnTapModel? secondModel, IconWithTitleAndOnTapModel? thirdModel}) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [
+            Styles.cardGradient1,
+            Styles.cardGradient2,
+          ]),
+          borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0).copyWith(bottom: 0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 30,
+              width: 7,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [
+                    Styles.accountDetailSmallSizeGradient1,
+                    Styles.accountDetailSmallSizeGradient2,
+                  ]),
+                  borderRadius: const BorderRadius.only(topRight: Radius.circular(10), bottomRight: Radius.circular(10))),
+            ),
+            const SizedBox(
+              width: 19,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: CommonText(
+                      text: title,
+                      fontSize: 18,
+                      letterSpacing: .2,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  iconWithTitleWidget(model: firstModel ?? IconWithTitleAndOnTapModel()),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  iconWithTitleWidget(model: secondModel ?? IconWithTitleAndOnTapModel()),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  iconWithTitleWidget(model: thirdModel ?? IconWithTitleAndOnTapModel(), isBottomBorderEnable: false),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget iconWithTitleWidget({required IconWithTitleAndOnTapModel model, bool isBottomBorderEnable = true}) {
+    return InkWell(
+      onTap: model.onTap,
+      focusColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(7),
+            decoration: BoxDecoration(color: Styles.iconCircleColor, shape: BoxShape.circle),
+            child: Image.asset(
+              model.assetPath,
+              height: 13,
+              width: 13,
+            ),
+          ),
+          const SizedBox(
+            width: 11,
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(
+                bottom: 18,
+              ),
+              decoration: BoxDecoration(border: Border(bottom: BorderSide(color: isBottomBorderEnable ? Colors.white : Colors.transparent))),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CommonText(
+                      text: model.title,
+                      fontSize: 14,
+                      letterSpacing: .2,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 18.0),
+                    child: Icon(Icons.arrow_forward_ios_outlined),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -318,4 +505,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
+}
+
+class IconWithTitleAndOnTapModel {
+  String title = "", assetPath = "";
+  Function()? onTap;
+
+  IconWithTitleAndOnTapModel({this.assetPath = "", this.title = "", this.onTap});
 }
