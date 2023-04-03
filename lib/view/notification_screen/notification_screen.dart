84f6905a0c48481210709cd680476fcce73909f4
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2023-04-03 13:28:12 +0530

    some improvements in notification backend

--- lib/view/notification_screen/notification_screen.dart
+++ lib/view/notification_screen/notification_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:okoto/backend/notification/notification_controller.dart';
 import 'package:okoto/backend/notification/notification_provider.dart';
+import 'package:okoto/backend/user/user_provider.dart';
 import 'package:okoto/model/notification/notification_model.dart';
 import 'package:okoto/utils/my_print.dart';
 import 'package:okoto/view/common/components/common_cachednetwork_image.dart';
@@ -26,19 +27,21 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  late UserProvider userProvider;
   late NotificationProvider notificationProvider;
   late NotificationController notificationController;
 
   bool isLoading = false;
-  NotificationModel notificationModel = NotificationModel(
-      title: 'New Game Arrived',
-      image: "https://res.cloudinary.com/dxegfkhzd/image/upload/v1677566819/pexels-sound-on-3761118_v4fec1.jpg",
-      description: 'we are happy to inform you about our newly launched game which is blab ba blabal ');
+  /*NotificationModel notificationModel = NotificationModel(
+    title: 'New Game Arrived',
+    image: "https://res.cloudinary.com/dxegfkhzd/image/upload/v1677566819/pexels-sound-on-3761118_v4fec1.jpg",
+    description: 'we are happy to inform you about our newly launched game which is blab ba blabal ',
+  );*/
   Future? getFuture;
 
   Future<void> getData() async {
     try {
-      await notificationController.getNotificationList();
+      await notificationController.getNotificationList(userId: userProvider.getUserId());
     } catch (e, s) {
       MyPrint.printOnConsole("Error in getData: $e");
       MyPrint.printOnConsole(s);
@@ -48,6 +51,7 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   void initState() {
     super.initState();
+    userProvider = context.read<UserProvider>();
     notificationProvider = context.read<NotificationProvider>();
     notificationController = NotificationController(notificationProvider: notificationProvider);
     getFuture = getData();
@@ -68,7 +72,7 @@ class _NotificationScreenState extends State<NotificationScreen> {
               if (snapshot.connectionState == ConnectionState.done) {
                 return getMainWidget();
               } else {
-                return Center(child: const CommonLoader());
+                return const Center(child: CommonLoader());
               }
             },
           ),
@@ -81,28 +85,54 @@ class _NotificationScreenState extends State<NotificationScreen> {
     return Consumer(
       builder: (BuildContext context, NotificationProvider notificationProvider, _) {
         List<NotificationModel> notificationModelList = notificationProvider.getNotificationModelList;
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.zero,
-                itemCount: notificationModelList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  NotificationModel model = NotificationModel.fromMap(notificationModelList[index].toMap());
-                  String key = "";
-                  String month = "";
-                  if (model.createdTime != null) {
-                    key = "${model.createdTime!.toDate().month}${model.createdTime!.toDate().year}";
-                    month = model.createdTime!.toDate().month == DateTime
-                        .now()
-                        .month ? "This month" : DatePresentation.MMyyyy(model.createdTime!);
-                  }
-                  return getNotificationCard(model, model.isOpened, notificationProvider, key, month, index);
-                },
-              ),
-            ),
-          ],
+
+        MyPrint.printOnConsole("notificationProvider.notificationsMapWithMonthYear:${notificationProvider.notificationsMapWithMonthYear}");
+
+        return RefreshIndicator(
+          onRefresh: () async {
+            getFuture = getData();
+            setState(() {});
+          },
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemCount: notificationModelList.length,
+            itemBuilder: (BuildContext context, int index) {
+              NotificationModel model = NotificationModel.fromMap(notificationModelList[index].toMap());
+              String key = "";
+              String month = "";
+              if (model.createdTime != null) {
+                key = "${model.createdTime!.toDate().month}${model.createdTime!.toDate().year}";
+
+                DateTime now = DateTime.now();
+                month = model.createdTime!.toDate().year == now.year && model.createdTime!.toDate().month == now.month
+                    ? "This month"
+                    : DatePresentation.MMyyyy(model.createdTime!);
+              }
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  notificationProvider.notificationsMapWithMonthYear[key] == model.id
+                      ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                    child: CommonText(
+                      text: month,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      letterSpacing: .2,
+                    ),
+                  )
+                      : const SizedBox(),
+                  getNotificationCard(model, model.isOpened, notificationProvider, key, month, index),
+                  const Divider(
+                    color: Colors.white,
+                    height: 1,
+                  ),
+                ],
+              );
+            },
+          ),
         );
       },
     );
@@ -112,6 +142,7 @@ class _NotificationScreenState extends State<NotificationScreen> {
     String titleFirstLetter = notificationModel.title.isNotEmpty ? notificationModel.title.toUpperCase() : "N";
 
     return InkWell(
+      highlightColor: Colors.transparent,
       onTap: () {
         // notificationModel.isOpened = !notificationModel.isOpened;
         // if(isSelected){
@@ -119,89 +150,69 @@ class _NotificationScreenState extends State<NotificationScreen> {
         // }
         if (isSelected) {
           notificationProvider.getNotificationModelList[index].isOpened = false;
-          notificationController.updateNotificationIsNotificationOpen(model: notificationModel, value: false);
+          notificationController.updateNotificationIsNotificationOpen(model: notificationModel, loggedInUserId: userProvider.getUserId(), value: false);
           setState(() {});
         }
       },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          notificationProvider.notificationsMapWithMonthYear[key] == notificationModel.id
-              ? Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: CommonText(
-              text: month,
-              fontWeight: FontWeight.w600,
-              fontSize: 20,
-              letterSpacing: .2,
-            ),
-          )
-              : const SizedBox(),
-          Container(
+      child: Container(
+        decoration: BoxDecoration(
+          color: isSelected ? Styles.notificationCardColor : Colors.transparent,
+        ),
+        child: ListTile(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          leading: Container(
+            width: 60,
+            height: 60,
             decoration: BoxDecoration(
-              color: isSelected ? Styles.notificationCardColor : Colors.transparent,
+              color: Styles.notificationImage,
+              borderRadius: BorderRadius.circular(4),
             ),
-            child: ListTile(
-              contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              leading: Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Styles.notificationImage,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                child: notificationModel.image.isNotEmpty
-                    ? CommonCachedNetworkImage(imageUrl: notificationModel.image, borderRadius: 4)
-                    : Center(
-                  child: Text(
-                    titleFirstLetter.characters.first,
-                    style: TextStyle(
-                      //color: Colors.black.withOpacity(.6),
-                        color: Styles.readMoreTextColor,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 23),
-                  ),
-                ),
+            child: notificationModel.image.isNotEmpty
+                ? CommonCachedNetworkImage(imageUrl: notificationModel.image, borderRadius: 4)
+                : Center(
+              child: Text(
+                titleFirstLetter.characters.first,
+                style: TextStyle(
+                  //color: Colors.black.withOpacity(.6),
+                    color: Styles.readMoreTextColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23),
               ),
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 1.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: CommonText(
-                        text: notificationModel.title,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 6,
-                    ),
-                    CommonText(
-                      text:  notificationModel.createdTime == null ? '' : DateFormat("dd/MM/yy").format(notificationModel.createdTime!.toDate()),
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ],
-                ),
-              ),
-              subtitle: notificationModel.description.isNotEmpty ? Padding(
-                padding: const EdgeInsets.only(top: 2.0),
-                child: CommonText(
-                  text: notificationModel.description,
-                  fontSize: 14.0,
-                  maxLines: 2,
-                  height: 1.1,
-                  textOverFlow: TextOverflow.ellipsis,
-                ),
-              ):SizedBox.shrink(),
             ),
           ),
-          const Divider(
-            color: Colors.white,
-            height: 1,
-          )
-        ],
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: CommonText(
+                    text: notificationModel.title,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                CommonText(
+                  text:  notificationModel.createdTime == null ? '' : DateFormat("dd/MM/yy").format(notificationModel.createdTime!.toDate()),
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ],
+            ),
+          ),
+          subtitle: notificationModel.description.isNotEmpty ? Padding(
+            padding: const EdgeInsets.only(top: 2.0),
+            child: CommonText(
+              text: notificationModel.description,
+              fontSize: 14.0,
+              maxLines: 2,
+              height: 1.1,
+              textOverFlow: TextOverflow.ellipsis,
+            ),
+          ):const SizedBox.shrink(),
+        ),
       ),
     );
   }
