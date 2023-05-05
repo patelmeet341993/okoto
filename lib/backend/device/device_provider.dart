17fcc2a03f755b1436fb0739e83bc4d9933d06f6
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2023-05-05 10:47:40 +0530

    In DeviceProvider,
    1) added runningDeviceId, runningDeviceModel, runningDeviceStreamSubscription and runningDeviceUpdationTimer variables
    2) added resetRunningDeviceData

--- lib/backend/device/device_provider.dart
+++ lib/backend/device/device_provider.dart
@@ -1,7 +1,30 @@
+import 'dart:async';
+
 import 'package:okoto/backend/common/common_provider.dart';
 import 'package:okoto/model/device/device_model.dart';
 
+import '../../model/device/running_device_model.dart';
+
 class DeviceProvider extends CommonProvider {
+  DeviceProvider() {
+    runningDeviceId = CommonProviderPrimitiveParameter<String>(
+      value: "",
+      notify: notify,
+    );
+    runningDeviceModel = CommonProviderPrimitiveParameter<RunningDeviceModel?>(
+      value: null,
+      notify: notify,
+    );
+    runningDeviceStreamSubscription = CommonProviderPrimitiveParameter<StreamSubscription<RunningDeviceModel?>?>(
+      value: null,
+      notify: notify,
+    );
+    runningDeviceUpdationTimer = CommonProviderPrimitiveParameter<Timer?>(
+      value: null,
+      notify: notify,
+    );
+  }
+
   //region User Devices
   //region User Device Models List
   final List<DeviceModel> _userDevices = <DeviceModel>[];
@@ -38,8 +61,26 @@ class DeviceProvider extends CommonProvider {
   //endregion
   //endregion
 
+  late CommonProviderPrimitiveParameter<String> runningDeviceId;
+  late CommonProviderPrimitiveParameter<RunningDeviceModel?> runningDeviceModel;
+  late CommonProviderPrimitiveParameter<StreamSubscription<RunningDeviceModel?>?> runningDeviceStreamSubscription;
+  late CommonProviderPrimitiveParameter<Timer?> runningDeviceUpdationTimer;
+
+  void resetRunningDeviceData() {
+    runningDeviceId.set(value: "", isNotify: false);
+    runningDeviceModel.set(value: null, isNotify: false);
+
+    runningDeviceStreamSubscription.get()?.cancel();
+    runningDeviceStreamSubscription.set(value: null, isNotify: false);
+
+    runningDeviceUpdationTimer.get()?.cancel();
+    runningDeviceUpdationTimer.set(value: null, isNotify: false);
+  }
+
   void resetAllData() {
     setUserDevices(devices: [], isNotify: false);
     setIsUserDevicesLoading(value: false, isNotify: false);
+
+    resetRunningDeviceData();
   }
 }
